2a8f10ae995fa50fae2b6978b4b966069cb73dbe
jyt94 <jhodinger@gmail.com>
2018-06-26 12:21:11 +0800

    Initial Commit

---
+++ test/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  test
+//
+//  Created by Yuntao Jiang on 2018/6/26.
+//  Copyright © 2018年 jyt94. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
